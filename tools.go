730cfacdf23cbd8a6138c74f93936a9dc3defd89
hanny21 <hana.klingova@gmail.com>
2023-01-06 11:17:43 +0100

    ci: fix tools

--- tools.go
+++ tools.go
@@ -18,6 +18,8 @@
 package indykite
 
 import (
+	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/daixiang0/gci"
+	_ "github.com/golang/mock/mockgen"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 )
