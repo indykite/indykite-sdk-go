596b8d392f444592fcf750b439d780d0351d422b
indykite <66900860+indykiteone@users.noreply.github.com>
2023-12-21 10:09:46 +0200

    chore(master): release 0.21.0

--- knowledge/doc.go
+++ knowledge/doc.go
@@ -17,4 +17,4 @@ Package knowledge implements the IndyKite Identity Knowledge API Client.
 */
 package knowledge
 
-const versionClient = "v0.20.0" // x-release-please-version
+const versionClient = "v0.21.0" // x-release-please-version
