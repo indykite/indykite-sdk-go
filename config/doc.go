a2922030b2806bde912079afe7a7a0a6ca184415
gg <111390039+cowan-macady@users.noreply.github.com>
2024-02-09 09:59:33 +0100

    Update doc.go

--- config/doc.go
+++ config/doc.go
@@ -15,4 +15,4 @@
 // Package config implement the Configuration Management Client.
 package config
 
-const versionClient = "v1.0.0" // x-release-please-version
+const versionClient = "v0.22.0" // x-release-please-version
