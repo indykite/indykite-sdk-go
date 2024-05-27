057d8443c0f9b6eaeb97c53ab5a451a88ffa3929
indykite <66900860+indykiteone@users.noreply.github.com>
2024-05-27 09:55:08 +0200

    chore(master): release 0.26.0

--- identity/doc.go
+++ identity/doc.go
@@ -15,4 +15,4 @@
 // Package identity implements the IndyKite Identity Service API Client.
 package identity
 
-const versionClient = "v0.25.0" // x-release-please-version
+const versionClient = "v0.26.0" // x-release-please-version
