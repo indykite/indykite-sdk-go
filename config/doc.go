a42a8f26260f6ff292b9e7fa729903738ffafa06
indykite <66900860+indykiteone@users.noreply.github.com>
2024-10-18 14:46:42 +0300

    chore(master): release 0.44.0

--- config/doc.go
+++ config/doc.go
@@ -15,4 +15,4 @@
 // Package config implement the Configuration Management Client.
 package config
 
-const versionClient = "v0.43.0" // x-release-please-version
+const versionClient = "v0.44.0" // x-release-please-version
