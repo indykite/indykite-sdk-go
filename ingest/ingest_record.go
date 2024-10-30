5ac2e4f681989a8b2197259d28dddf1f3378a34e
Cowan Macady <cowan.macady@gmail.com>
2024-10-30 08:45:40 +0100

    fix: add loop to delete AppSpace
    implement ENG-4927

--- ingest/ingest_record.go
+++ ingest/ingest_record.go
@@ -29,7 +29,7 @@ func (c *Client) IngestRecord(
 	opts ...grpc.CallOption,
 ) (*ingestpb.IngestRecordResponse, error) {
 	ctx = insertMetadata(ctx, c.xMetadata)
-	resp, err := c.client.IngestRecord( //nolint:staticcheck // backward compatibility.
+	resp, err := c.client.IngestRecord( //nolint:staticcheck // deprecated.
 		ctx, &ingestpb.IngestRecordRequest{
 			Record: record,
 		}, opts...)
