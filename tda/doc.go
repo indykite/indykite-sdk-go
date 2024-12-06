83594c42f56e1013dc2962823a7d767cdadd3c3a
indykite <66900860+indykiteone@users.noreply.github.com>
2024-12-06 10:48:26 +0200

    chore(master): release 0.48.1

--- tda/doc.go
+++ tda/doc.go
@@ -17,4 +17,4 @@ Package tda implements the IndyKite TrustedDataAccess Service API Client.
 */
 package tda
 
-const versionClient = "v0.48.0" // x-release-please-version
+const versionClient = "v0.48.1" // x-release-please-version
