f8224bc2eda51577c7330e28307b2b6f65b16fa9
indykite <66900860+indykiteone@users.noreply.github.com>
2025-02-26 10:13:32 +0200

    chore(master): release 0.51.0

--- authorization/doc.go
+++ authorization/doc.go
@@ -15,4 +15,4 @@
 // Package authorization implements the IndyKite Authorization Service API Client.
 package authorization
 
-const versionClient = "v0.50.0" // x-release-please-version
+const versionClient = "v0.51.0" // x-release-please-version
