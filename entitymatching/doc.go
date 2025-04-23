37fce3edd80459b4968c1cbc87f5dcf69408bbdd
indykite <66900860+indykiteone@users.noreply.github.com>
2025-04-23 09:39:50 +0200

    chore(master): release 0.55.0

--- entitymatching/doc.go
+++ entitymatching/doc.go
@@ -17,4 +17,4 @@ Package entitymatching implements the IndyKite TrustedDataAccess Service API Cli
 */
 package entitymatching
 
-const versionClient = "v0.54.0" // x-release-please-version
+const versionClient = "v0.55.0" // x-release-please-version
