a345d01571c46b1041626f8ea3cf3f3bf196f6eb
indykite <66900860+indykiteone@users.noreply.github.com>
2025-10-10 05:02:34 -0600

    chore(master): release 0.68.0
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ingest/doc.go
+++ ingest/doc.go
@@ -17,4 +17,4 @@ Package ingest implements the IndyKite Ingest Service API Client.
 */
 package ingest
 
-const versionClient = "v0.67.1" // x-release-please-version
+const versionClient = "v0.68.0" // x-release-please-version
